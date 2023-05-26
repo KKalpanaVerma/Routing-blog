e20077db01108cad2f6c09d34693f767208c2739
TechKalpana <vkalpana45678@gmail.com>
2023-05-26 09:43:15 +0530

    comit26-may-2023

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
-import { NgModule } from '@angular/core';
+import {NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'user',
+    component:UserComponent
+  },
+  {
+    path:'about',
+    component:AboutComponent
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
